b3bf7abbb56cb096a7c0eabcd4f799d906b134f4
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2025-08-29 21:10:21 +0500

    feat: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversed = String(Math.abs(n)).split('').reverse().join('');
+  return Number(reversed);
 };
